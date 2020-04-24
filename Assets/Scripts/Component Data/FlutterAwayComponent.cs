92d6d5901b4ce599982b5c3cbcca14f780895bd3
ClaireIrou <c.iroudayassamy@gmail.com>
2020-04-24 16:24:18 -0400

    Flutter Away functionality
    ...that exists in the void of unhooked code.
    Untested and called by nothing at this point.

---
+++ Assets/Scripts/Component Data/FlutterAwayComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct FlutterAwayComponent : IComponentData
+{
+    public float3 target;
+    public float speed;
+    public bool disabled;
+
+}
---
+++ Assets/Scripts/Systems/FlutterAwaySystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Jobs;
+using Unity.Collections;
+
+public class FlutterAwaySystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+
+        var jobHandle = Entities
+           .WithName("FlutterAwaySystem")
+           .ForEach((ref Translation position, ref FlutterAwayComponent fAwayComp) =>
+           {
+               position.Value += fAwayComp.target;
+               if(math.distance(position.Value, fAwayComp.target) < 1)
+               {
+                   // destroy entity - not sure how to do this
+               }
+           })
+            .Schedule(inputDeps);
+
+        return jobHandle;
+
+    }
+}
