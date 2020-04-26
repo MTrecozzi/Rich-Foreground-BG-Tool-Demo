e59ba095df25e65f48aac4b077157f7e33a86c6b
ClaireIrou <c.iroudayassamy@gmail.com>
2020-04-26 07:35:43 -0400

    Sine curve movement for butterflies
    Small bugs to fix:
    -adjusting sin amp if entity dips below foreground
    -some bounce while waiting, some don't (no idea why)

---
+++ Assets/Scripts/Component Data/SineCurveComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct SineCurveComponent : IComponentData
+{
+    public float frequency;
+    public float amp;
+    public float y0;
+    public float startTime;
+
+}
---
+++ Assets/Scripts/Systems/SineMoveSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Jobs;
+using Unity.Collections;
+
+public class SineMoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+        float age = UnityEngine.Time.time - deltaTime;
+
+        var jobHandle = Entities
+            .WithName("SineMoveSystem")
+            .ForEach((ref Translation position, ref SineCurveComponent sinComp) =>
+            {
+                sinComp.y0 = position.Value.y;
+                
+                float sin = Mathf.Sin(Mathf.PI * .5f * age / sinComp.amp);
+                position.Value.y = sinComp.y0 + sinComp.frequency * sin;
+
+
+            })
+            .Schedule(inputDeps);
+
+        return jobHandle;
+    }
+}
